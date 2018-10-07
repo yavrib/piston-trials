8ef19298dca1a4963af5e11f9a1ff6b71aeb7b02
Berkan Yavri <berkan.yavri@deliveryhero.com>
2018-10-07 16:14:40 +0200

    Are we game yet?

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,39 @@
 extern crate piston;
 extern crate piston_window;
 extern crate find_folder;
+extern crate sdl2_window;
 
+use sdl2_window::*;
 use piston::input::*;
 use piston::event_loop::*;
 use piston_window::*;
 
+struct Rectangle {
+    color: [f64; 4],
+    shape: [f64; 4],
+}
+
+struct Color {
+    r: f32,
+    g: f32,
+    b: f32,
+    a: f32,
+}
+
+struct Shape {
+    color: Color,
+    x: f64,
+    y: f64,
+    height: f64,
+    width: f64,
+}
+
 fn main() {
-    let mut window: PistonWindow =
-        WindowSettings::new("Hello Piston!", [640, 480])
-        .exit_on_esc(true).build().unwrap();
+    let mut window: PistonWindow<Sdl2Window> =
+        WindowSettings::new("Hello Piston!", [1000, 800])
+            .resizable(false)
+            .fullscreen(true)
+            .exit_on_esc(true).build().unwrap();
     let mut events = Events::new(EventSettings::new().lazy(true));
     let assets = find_folder::Search::ParentsThenKids(3, 3)
         .for_folder("assets").unwrap();
@@ -19,26 +43,48 @@ fn main() {
 
     window.set_capture_cursor(false);
 
-    let mut message = "Hello";
+    let mut message = String::from("");
+    let mut cursor: [f64; 2] = [0f64, 0f64];
+    let mut rectanglee = Shape {
+        color: Color {
+            r: 0.0,
+            g: 0.0,
+            b: 0.0,
+            a: 1.0,
+        },
+        x: 0.0,
+        y: 0.0,
+        height: 100.0,
+        width: 100.0,
+    };
 
     while let Some(e) = events.next(&mut window) {
         window.draw_2d(&e, |c, g| {
             clear([1.0; 4], g);
-            rectangle([0.0, 0.0, 0.0, 1.0], // red
-                      [0.0, 0.0, 90.0, 90.0],
+            rectangle([rectanglee.color.r, rectanglee.color.g, rectanglee.color.b, rectanglee.color.a],
+                      [rectanglee.x - (rectanglee.width / 2f64), rectanglee.y - (rectanglee.height / 2f64), rectanglee.height, rectanglee.width],
                       c.transform, g);
+        });
+
+        e.mouse_cursor(|x, y| {
+            cursor = [x, y];
+            println!("{:?} {:?}", x, y);
         });
 
         if let Some(Button::Mouse(button)) = e.press_args() {
             println!("Pressed mouse button '{:?}'", button);
-            message = "Left Button Pressed"
+            if button == MouseButton::Left {
+                message = format!("Left, {:?}, {:?}", cursor[0], cursor[1]);
+                rectanglee.x = cursor[0];
+                rectanglee.y = cursor[1];
+            }
         }
 
         window.draw_2d(&e, |c, g| {
             let transform = c.transform.trans(100.0, 100.0);
             // Set a white background
             text::Text::new_color([0.0, 0.0, 0.0, 1.0], 32).draw(
-                message,
+                &message,
                 &mut glyphs,
                 &c.draw_state,
                 transform, g
